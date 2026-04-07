df7397e5674f5d9b97edb17eb22f927557b8d42f
Jaimayal <jaime.ayalaa.76@gmail.com>
2026-04-07 17:40:53 -0600

    Refactor dashboard layout and add investment modal

--- resources/views/pages/⚡dashboard.blade.php
+++ resources/views/pages/⚡dashboard.blade.php
@@ -4,12 +4,32 @@ use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
-new #[Title('Dashboard')] class extends Component
-{
+new #[Title('Dashboard')] class extends Component {
     //
 };
 ?>
 
-<div>
-    {{-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk --}}
+<div class="flex flex-col space-y-4">
+    <flux:heading size="xl">Dashboard</flux:heading>
+    <flux:modal.trigger name="edit-profile">
+        <flux:button variant="primary" class="w-fit">Agregar inversion</flux:button>
+    </flux:modal.trigger>
+    <flux:modal name="edit-profile" class="md:w-96">
+        <div class="space-y-6">
+            <div>
+                <flux:heading size="lg">Agregar inversion</flux:heading>
+                <flux:text class="mt-2">Agrega los datos de la nueva inversión.</flux:text>
+            </div>
+            <flux:input label="Nombre" placeholder="Nombre de la inversión" />
+            <flux:input.group label="Monto">
+                <flux:input.group.prefix>$</flux:input.group.prefix>
+                <flux:input mask:dynamic="$money($input)" placeholder="0.00" />
+            </flux:input.group>
+            <flux:input label="Fecha de inicio" type="date" />
+            <div class="flex">
+                <flux:spacer />
+                <flux:button type="submit" variant="primary">Agregar</flux:button>
+            </div>
+        </div>
+    </flux:modal>
 </div>
