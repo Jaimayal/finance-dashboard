574f9eb6eca17851644250d2248101b5aa03fb62
fernandoAlco <fernandoalco56@gmail.com>
2026-04-07 18:13:52 -0500

    Componente livewire dashboard

---
+++ resources/views/pages/⚡dashboard.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+new #[Title('Dashboard')] class extends Component
+{
+    //
+};
+?>
+
+<div>
+    {{-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk --}}
+</div>
